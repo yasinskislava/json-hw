91a84d6df88914b937fe4c9c1f5faa739b076720
yasinskislava <yasinskislava@gmail.com>
2024-01-20 21:08:59 +0200

    save

--- src/index.js
+++ src/index.js
@@ -8,23 +8,27 @@ const backdrop = document.querySelector(".backdrop");
 const info = document.querySelectorAll(".card-info");
 const list = document.querySelector(".list");
 const title = document.querySelector("#title");
+const close = document.querySelector("#close");
+const search = document.querySelector("#search");
+const searchbar = document.querySelector(".searchbar");
+const searchbarInput = document.querySelector("#searchbarInput");
 let index = -1;
 let jsonData = students;
 
-function reloadCards() {
+function reloadCards(arr) {
     for (const i of info) {
       i.value = "";
     }
     list.innerHTML = "";
-    for (let i = 0; i < cardsArr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
       list.insertAdjacentHTML(
         "beforeend",
         `<li class="item" id="${i}">
-                <p>Name: ${cardsArr[i].name}</p>
-                <p>Surname: ${cardsArr[i].surname}</p>
-                <p>Age: ${cardsArr[i].age}</p>
-                <p>Course: ${cardsArr[i].course}</p>
-                <p>Faculty: ${cardsArr[i].faculty}</p>
+                <p>Name: ${arr[i].name}</p>
+                <p>Surname: ${arr[i].surname}</p>
+                <p>Age: ${arr[i].age}</p>
+                <p>Course: ${arr[i].course}</p>
+                <p>Faculty: ${arr[i].faculty}</p>
             </li>`
       );
     }
@@ -40,16 +44,22 @@ let cardsArr = [];
 submit.addEventListener("click", (e) => {
     e.preventDefault();
     backdrop.classList.add("off");
-    if (title.textContent === "Add Card") {
+    if (
+      info[0].value.trim() !== "" &&
+      info[1].value.trim() !== "" &&
+      info[2].value.trim() !== "" &&
+      info[3].value.trim() !== "" &&
+      info[4].value.trim() !== ""
+    ) {
+        if (title.textContent === "Add Card") {
         cardsArr.push({
           name: info[0].value,
           surname: info[1].value,
           age: info[2].value,
           course: info[3].value,
-          faculty: info[4].value
+          faculty: info[4].value,
         });
-    }
-    else if (title.textContent === "Edit Card") {
+      } else if (title.textContent === "Edit Card") {
         const cards = document.querySelectorAll(".item");
         cardsArr[index] = {
           name: info[0].value,
@@ -58,9 +68,13 @@ submit.addEventListener("click", (e) => {
           course: info[3].value,
           faculty: info[4].value,
         };
+        }
+        jsonData = JSON.stringify(cardsArr);
     }
-    jsonData = JSON.stringify(cardsArr);
-    reloadCards();
+    else {
+        alert("Заповніть всі поля");
+    }
+    reloadCards(cardsArr);
 });
 
 window.addEventListener("keydown", (e) => {
@@ -88,10 +102,22 @@ minus.addEventListener("click", () => {
         i.addEventListener("click", () => {
             index = i.id;
             cardsArr.splice(index, index + 1);
-            reloadCards();
+            reloadCards(cardsArr);
             jsonData = JSON.stringify(cardsArr);
         });
     }
 });
 
-
+search.addEventListener("click", () => {
+    searchbar.classList.toggle("vis");
+});
+close.addEventListener("click", () => {
+    searchbar.classList.toggle("vis");
+});
+function searchCheck(obj) {
+    return obj.surname.includes(searchbarInput.value.trim())
+}
+searchbarInput.addEventListener("input", e => {
+    const tempArr = cardsArr.filter(searchCheck);
+    reloadCards(tempArr);
+});
